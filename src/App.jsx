ec04e9b893c4b3c26353cb551e7849e1f29470d4
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-27 00:16:06 -0300

    instalando bootstrap y dandole estructura al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <h1>Alta de Películas</h1>
+      <header>
+        <h4 className="text-center my-3">
+          Alta de Películas
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Clapper%20Board.png"
+            alt="Clapper Board"
+            className="iconClapperBoard"
+          />
+        </h4>
+        <hr />
+      </header>
+      <main></main>
+      <footer>
+        <hr />
+        <h6 className="text-center my-3">
+          &copy; todos los derechos reservados
+        </h6>
+      </footer>
     </>
   );
 }
